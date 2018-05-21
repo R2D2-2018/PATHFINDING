e2ae1ebb1a06c66ee254db409c0d82c512a3ea22
Julian van Doorn <gigajulian@gmail.com>
2018-05-21 19:02:43 +0200

    Made queue circular and documented it

--- src/queue.hpp
+++ src/queue.hpp
@@ -9,24 +9,69 @@
 #define QUEUE_HPP
 
 namespace Pathfinding {
+    /**
+     * @brief Simple queue class
+     * 
+     * @tparam T Type of the elements placed in this queue
+     * @tparam L Length of this queue
+     */
     template <class T, int L>
     class queue {
     private:
         T elements[L];
         T* head;
         T* tail;
+        int elementCount;
 
     public:
-        queue(): head(elements), tail(elements) { }
+        /**
+         * @brief Construct a new queue object
+         * 
+         */
+        queue(): head(elements), tail(elements), elementCount(0) { }
 
+        /**
+         * @brief Places a variable of type T in the queue
+         * 
+         * @param v Variable to enqueue
+         */
         void enqueue(T v) {
-            *tail++ = v;
+            if (elementCount < L) {
+                elementCount++;
+
+                *tail++ = v;
+
+                if (tail > elements + L) {
+                    tail = elements;
+                }
+            }
         }
 
+        /**
+         * @brief Takes a variable of type T out of the queue
+         * 
+         * @return T Variable dequeued
+         */
         T dequeue() {
-            return *head++;
+            if (elementCount > 0) {
+                elementCount--;
+
+                T v = *head++;
+
+                if (head > elements + L) {
+                    head = elements;
+                }
+
+                return v;
+            }
         }
 
+        /**
+         * @brief Returns whether this queue is empty or not
+         * 
+         * @return true 
+         * @return false 
+         */
         bool empty() {
             return head == tail;
         }
