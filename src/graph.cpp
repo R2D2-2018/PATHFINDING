80d5d5aec86f494a22d947a8e24a4a8dbb1ef627
Julian van Doorn <gigajulian@gmail.com>
2018-05-21 18:10:34 +0200

    Implemented graph datatype

--- src/graph.cpp
+++ src/graph.cpp
@@ -7,12 +7,42 @@
 
 #include "graph.hpp"
 
-namespace Pathfinding {
-    Graph::Graph() {
+#include <iostream>
 
+namespace Pathfinding {
+    Graph::Graph(Node* nodes, int nodeCount, Node** cumulativeEdges, int cumulativeEdgesCount) : 
+        nodes(nodes),
+        nodeCount(nodeCount),
+        cumulativeEdges(cumulativeEdges),
+        cumulativeEdgesCount(cumulativeEdgesCount) 
+    {}
+
+    void Graph::getNodes(Node** nodes, int* len) {
+        *nodes = this->nodes;
+        *len = this->nodeCount;
     }
 
-    void Graph::getNodes(Node** nodes, int& len) {
-        
+    Node* Graph::getNodes() {
+        return nodes;
+    }
+
+    void Graph::setAlgorithm(PathfindingAlgorithm& alg) {
+        this->alg = &alg;
+    }
+
+    void Graph::reset() {
+        Node* _nodes = nodes;
+        int _nodeCount = nodeCount;
+
+        while (_nodeCount > 0) {
+            _nodes->setState(0);
+            _nodes++;
+            _nodeCount--;
+        }
+    }
+
+    PathfindingAlgorithm::SuccessState Graph::findPath(Node& source, Node& dest, Node** path, int len, int* travelled) {
+        reset();
+        return alg->findPath(source, dest, path, len, travelled);
     }
 } // namespace Pathfinding
--- src/graph.hpp
+++ src/graph.hpp
@@ -9,16 +9,29 @@
 #define GRAPH_HPP
 
 #include "node.hpp"
+#include "pathfinding_algorithm.hpp"
 
 namespace Pathfinding {
     class Graph {
     private:
-        Node** nodes;
+        Node* nodes;
+        int nodeCount;
         Node** cumulativeEdges; // all edges combined
+        int cumulativeEdgesCount;
+        PathfindingAlgorithm* alg;
 
     public:
-        Graph();
-        void getNodes(Node** nodes, int& len);
+        Graph(Node* nodes, int nodeCount, Node** cumulativeEdges, int cumulativeEdgesCount);
+        void getNodes(Node** nodes, int* len);
+        Node* getNodes();
+        void setAlgorithm(PathfindingAlgorithm& alg);
+        void reset();
+        PathfindingAlgorithm::SuccessState findPath(Node& source, Node& dest, Node** path, int len, int* travelled);
+
+        template <class T>
+        friend T& operator<< (T& os, const Graph& n) {
+            return os << "Graph " << (void*) &n;
+        }
     };
 } // namespace Pathfinding
 
