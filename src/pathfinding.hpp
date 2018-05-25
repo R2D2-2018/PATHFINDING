ff0bbe4d9000e211b78cc7a5b04ee1d1300c94f5
Julian van Doorn <gigajulian@gmail.com>
2018-05-25 19:29:33 +0200

    Clang-formatted

--- src/pathfinding.hpp
+++ src/pathfinding.hpp
@@ -24,20 +24,20 @@ extern PathfindingAlgorithm *algorithms[1];
 
 /**
  * @brief Finds a path from source to destination using the given algorithm.
- * 
+ *
  * @details
  * This function internally sets up datatypes for pathfinding.
- * 
+ *
  * @param[in] source Source node identifier
  * @param[in] dest Destination node identifier
  * @param[in] algorithmId Pathfinding algorithm identifier
- * @return Path 
+ * @return Path
  */
 Path calculatePath(NodeId source, NodeId dest, AlgorithmId algorithmId);
 
 /**
  * @brief Adds a node to the internal graph used for pathfinding.
- * 
+ *
  * @param nodeId[in] Id of node to add
  * @return true On success
  * @return false On failure (memory full, id already used, etc.)
@@ -46,7 +46,7 @@ bool addNode(NodeId nodeId);
 
 /**
  * @brief Removes a node from the internal graph used for pathfinding.
- * 
+ *
  * @param nodeId[in] Id of node to remove
  * @return true On success
  * @return false On failure (node does not exist, etc.)
@@ -55,7 +55,7 @@ bool removeNode(NodeId nodeId);
 
 /**
  * @brief Adds a strong edge from node 0 to node 1 and back.
- * 
+ *
  * @param node0[in] First node
  * @param node1[in] Second node
  * @return true On success
@@ -65,7 +65,7 @@ bool addEdge(NodeId node0, NodeId node1);
 
 /**
  * @brief Removes the edges from node 0 to node 1 and back.
- * 
+ *
  * @param node0[in] First node
  * @param node1[in] Second node
  * @return true On success
@@ -75,10 +75,10 @@ bool removeEdge(NodeId node0, NodeId node1);
 
 /**
  * @brief Adds a weak edge from node 0 to node 1.
- * 
+ *
  * @details
  * A weak edge means that the pathfinder can only travel one way over this node.
- * 
+ *
  * @param node0 First node
  * @param node1 Second node
  * @return true On success
@@ -88,10 +88,10 @@ bool addWeakEdge(NodeId node0, NodeId node1);
 
 /**
  * @brief Removes a weak edge from node 0 to node 1.
- * 
+ *
  * @details
  * A weak edge means that the pathfinder can only travel one way over this node.
- * 
+ *
  * @param node0 First node
  * @param node1 Second node
  * @return true On success
