84ad4013d042544479796e25f4f35a400cab72ef
Julian van Doorn <gigajulian@gmail.com>
2018-05-25 14:31:10 +0200

    Changed abstract interface

--- src/data_interface.hpp
+++ src/data_interface.hpp
@@ -9,12 +9,19 @@
 #define DATA_INTERFACE_HPP
 
 #include "graph.hpp"
+#include "path.hpp"
+#include <stdint.h>
 
 namespace Pathfinding {
 class DataInterface {
   public:
-    virtual Graph findPath() const = 0;
-    virtual void setMap(Graph map) = 0;
+    virtual Path calculatePath(NodeId source, NodeId dest, AlgorithmId algorithmId) = 0;
+    virtual bool addNode(NodeId nodeId) = 0;
+    virtual bool removeNode(NodeId nodeid) = 0;
+    virtual bool addEdge(NodeId node0, NodeId node1) = 0;
+    virtual bool removeEdge(NodeId node0, NodeId node1) = 0;
+    virtual bool addWeakEdge(NodeId node0, NodeId node1) = 0;
+    virtual bool removeWeakEdge(NodeId node0, NodeId node1) = 0;
 };
 } // namespace Pathfinding
 
