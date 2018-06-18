a62a0e4303397696d121647c4cd2506d656c2b95
Renske Kuip <renske.kuip@student.hu.nl>
2018-06-18 19:23:24 +0200

    Abstract path interface added

--- src/path.cpp
+++ src/path.cpp
@@ -1,1 +1,28 @@
-// empty file
+/**
+ * @file      path.cpp
+ * @brief     Path class definitions
+ * @author    Renske Kuip
+ * @license   See LICENSE
+ */
+
+#include "path.hpp"
+
+namespace Pathfinding {
+Path::Path() {
+}
+
+void Path::addNode(Node node) {
+} // enqueue
+
+Node *Path::getNode() const {
+} // dequeue zonder return
+
+uint32_t Path::getPathLen() const {
+}
+
+void Path::showPath() const {
+}
+
+void Path::showFirst() const {
+}
+} // namespace Pathfinding
--- src/path.hpp
+++ src/path.hpp
@@ -1,6 +1,58 @@
+/**
+ * @file      path.hpp
+ * @brief     Path class definitions
+ * @author    Renske Kuip
+ * @license   See LICENSE
+ */
+
 #ifndef PATH_HPP
 #define PATH_HPP
 
-class Path {};
+#include "node.hpp"
+#include "queue.hpp"
 
+// note: Node **const &path, uint32_t len,
+
+namespace Pathfinding {
+class Path {
+  private:
+    // uint32_t maxNodeCount;
+    // Node *path[32]; // 32 vaste waarde
+    queue<Node *, 32> path; // drive in the order of the queue
+
+  public:
+    /**
+     * @brief Empty constructor
+     */
+    Path();
+
+    /**
+     * @brief Add node at the end of the path
+     *
+     * @param Node node that will be added at the end
+     */
+    void addNode(Node node); // enqueue
+    /**
+     * @brief Remove first node of the path
+     *
+     */
+    Node *getNode() const; // dequeue zonder return
+    /**
+     * @brief Get the Path Length
+     *
+     * @return uinr32_t length of path
+     */
+    uint32_t getPathLen() const;
+    /**
+     * @brief Show entire path
+     *
+     */
+    void showPath() const;
+    /**
+     * @brief Show first node without removing it
+     *
+     */
+    void showFirst() const;
+};
+};     // namespace Pathfinding
 #endif // PATH_HPP
